ca0ecf0a8ebc249e7b173d662d3cc6297611a84e
Kat Munson <v-katmunson@microsoft.com>
2023-09-22 15:36:39 -0500

    Level 18 finish

--- Program.cs
+++ Program.cs
@@ -1,51 +1,59 @@
-using System.Collections;
-using System.Data.Common;
-using System.Reflection.Metadata.Ecma335;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Xml.Serialization;
-using Microsoft.VisualBasic;
-using Microsoft.Win32;
 
 namespace HelloWorld
 {
     class Program
     {
-        enum SoupType { Soup, Stew, Gumbo };
-        enum MainIngredient { Mushrooms, Chicken, Carrots, Potatoes };
-        enum Seasoning { Spicy, Salty, Sweet };
-
+        enum Arrowhead { Steel = 1, Wood = 2, Gold = 3};
+        enum Fletching { Plastic = 1, TurkeyFeathers = 2, GooseFeathers = 3 };
 
         static void Main(string[] args)
         {
-            SoupType souptype = SoupType.Soup;
-            MainIngredient mainIngredient = MainIngredient.Mushrooms;
-            Seasoning seasoning = Seasoning.Spicy;
+            float total = 0;
+            Console.WriteLine("Choose a fletching 1(plastic) 2(turkeyfeathers) 3(goodfeathers)");
+            string fletch = Console.ReadLine();
+            int fletchNum = Convert.ToInt32(fletch);
+            Console.WriteLine("Choose an arrowhead 1(steel) 2(wood) or 3(gold)");
+            string arrowh = Console.ReadLine();
+            int arrowhNum = Convert.ToInt32(arrowh); 
+            Console.WriteLine("Choose a shaft length (number)");
+            string len = Console.ReadLine();
+            int lenNum = Convert.ToInt32(len);
+            Arrow newArrow = new Arrow(arrowhNum, fletchNum, lenNum);
 
-            (SoupType soupt, MainIngredient main, Seasoning seas) soup = (souptype, mainIngredient, seasoning);
-
-            Console.WriteLine("Please choose a seasoning");
-            string seasoningInput = Console.ReadLine();
-            switch (seasoningInput)
-            {
-                case "spicy":
-                    soup.seas = Seasoning.Spicy;
-                    break;
-                case "salty":
-                    soup.seas= Seasoning.Salty;
-                    break;
-                case "sweet":
-                    soup.seas = Seasoning.Sweet;
-                    break;
-            }
-
-            DisplaySoup(soup);
+            Console.WriteLine($"Your cost is {newArrow.GetCost()}");
 
         }
 
-        static void DisplaySoup((SoupType soupt, MainIngredient mainingred, Seasoning season) soup)
+        class Arrow
         {
-            Console.WriteLine($"{soup.season} {soup.mainingred} {soup.soupt}");
+            public Arrowhead _arrowhead;
+            public int _arrowheadPos;
+            public Fletching _fletching;
+            public int _fletchingPos;
+            public int _length;
+
+            public double cost;
+
+            public int[] arrowheadCosts = new int[] {10, 3, 5};
+            public int[] fletchingCosts = new int[] {10, 5, 3};
+
+            public Arrow(int arrowhead, int fletching, int length)
+            {
+                _arrowhead = (Arrowhead)arrowhead;
+                _fletching = (Fletching)fletching;
+                _length = length;
+                _arrowheadPos = arrowhead;
+                _fletchingPos = fletching;
+            }
+            public double GetCost() {
+                cost += (this._length * 0.5);
+                cost += (this.arrowheadCosts[_arrowheadPos - 1]);
+                cost += (this.fletchingCosts[_fletchingPos - 1]);
+                return cost;
+                }
         }
     }
 }
---
+++ Soup.cs
@@ -0,0 +1,51 @@
+// using System.Collections;
+// using System.Data.Common;
+// using System.Reflection.Metadata.Ecma335;
+// using System.Security.Cryptography;
+// using System.Security.Cryptography.X509Certificates;
+// using System.Xml.Serialization;
+// using Microsoft.VisualBasic;
+// using Microsoft.Win32;
+
+// namespace HelloWorld
+// {
+//     class Program
+//     {
+//         enum SoupType { Soup, Stew, Gumbo };
+//         enum MainIngredient { Mushrooms, Chicken, Carrots, Potatoes };
+//         enum Seasoning { Spicy, Salty, Sweet };
+
+
+//         static void Main(string[] args)
+//         {
+//             SoupType souptype = SoupType.Soup;
+//             MainIngredient mainIngredient = MainIngredient.Mushrooms;
+//             Seasoning seasoning = Seasoning.Spicy;
+
+//             (SoupType soupt, MainIngredient main, Seasoning seas) soup = (souptype, mainIngredient, seasoning);
+
+//             Console.WriteLine("Please choose a seasoning");
+//             string seasoningInput = Console.ReadLine();
+//             switch (seasoningInput)
+//             {
+//                 case "spicy":
+//                     soup.seas = Seasoning.Spicy;
+//                     break;
+//                 case "salty":
+//                     soup.seas= Seasoning.Salty;
+//                     break;
+//                 case "sweet":
+//                     soup.seas = Seasoning.Sweet;
+//                     break;
+//             }
+
+//             DisplaySoup(soup);
+
+//         }
+
+//         static void DisplaySoup((SoupType soupt, MainIngredient mainingred, Seasoning season) soup)
+//         {
+//             Console.WriteLine($"{soup.season} {soup.mainingred} {soup.soupt}");
+//         }
+//     }
+// }
