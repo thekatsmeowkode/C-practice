fc6d203fe6c07fff3597c7d0de7f9069c31555bf
Kat Munson <v-katmunson@microsoft.com>
2023-09-22 14:04:22 -0500

    Complete soup exercise

--- Program.cs
+++ Program.cs
@@ -1,107 +1,51 @@
-﻿using System.Collections;
+using System.Collections;
+using System.Data.Common;
 using System.Reflection.Metadata.Ecma335;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Xml.Serialization;
+using Microsoft.VisualBasic;
+using Microsoft.Win32;
 
 namespace HelloWorld
 {
     class Program
     {
-        enum Chest { Open, Closed, Locked };
+        enum SoupType { Soup, Stew, Gumbo };
+        enum MainIngredient { Mushrooms, Chicken, Carrots, Potatoes };
+        enum Seasoning { Spicy, Salty, Sweet };
+
 
         static void Main(string[] args)
         {
-            // Console.BackgroundColor = ConsoleColor.DarkCyan;
-            
-            // Console.ForegroundColor = ConsoleColor.Black;
-            // Console.Title = "Beautiousness"; 
-            // Console.Beep();
-            // Console.WriteLine("\"");
-            // string name;
-            // Console.WriteLine("What is your name?");
-            // name = Console.ReadLine();
-            // Console.WriteLine("Hi " + name);
+            SoupType souptype = SoupType.Soup;
+            MainIngredient mainIngredient = MainIngredient.Mushrooms;
+            Seasoning seasoning = Seasoning.Spicy;
 
-            // Console.WriteLine("Target Row?");
-            // string row = Console.ReadLine();
-            // int numrow = Convert.ToInt32(row);
-            // Console.WriteLine("Target Column?");
-            // string col = Console.ReadLine();
-            // int numcol = Convert.ToInt32(col);
-            // Console.WriteLine($"({numrow}, {numcol - 1}) \n ({numrow - 1}, {numcol}) \n ({numrow}, {numcol + 1}) \n ({numrow + 1}, {numcol})");
+            (SoupType soupt, MainIngredient main, Seasoning seas) soup = (souptype, mainIngredient, seasoning);
 
-            // Console.WriteLine("Enter a number");
-            // string stringNum = Console.ReadLine();
-            // int chosenNum = Convert.ToInt32(stringNum);
-            // float chosenNumModulo = chosenNum % 2;
-            // if (chosenNumModulo > 0)
-            // {
-            //     Console.WriteLine("Odd");
-            // }
-            // else
-            // {
-            //     Console.WriteLine("Even");
-            // }
-            //     int result = AskForNumber("what is the airspeed velocity of an unladen swallow?");
-            //     Console.WriteLine(result); 
-
-            // }
-
-            // static int AskForNumber(string text)
-            // {
-            //     Console.WriteLine(text);
-            //     string strnum = Console.ReadLine();
-            //     int swallowNum = Convert.ToInt32(strnum);
-            //     return swallowNum;
-            // }
-            ChangeChest();
-        }
-
-        static Chest DetermineActionValidity(string currentRequest, Chest chestState)
+            Console.WriteLine("Please choose a seasoning");
+            string seasoningInput = Console.ReadLine();
+            switch (seasoningInput)
             {
-                
-                switch (currentRequest)
-                {
-                    case "lock":
-                        if (chestState == Chest.Closed)
-                        {
-                            return Chest.Locked;
-                        }
-                        break;
-                    case "close":
-                        if (chestState == Chest.Open)
-                        {
-                            return Chest.Closed;
-                        }
-                        break;
-                    case "unlock":
-                        if (chestState == Chest.Locked)
-                        {
-                            return Chest.Closed;
-                        }
-                        break;
-                    case "open":
-                        if (chestState == Chest.Closed)
-                        {
-                            return Chest.Open;
-                        }
-                        break;
-                    default:
-                        return chestState;
-                };
-                return chestState;
+                case "spicy":
+                    soup.seas = Seasoning.Spicy;
+                    break;
+                case "salty":
+                    soup.seas= Seasoning.Salty;
+                    break;
+                case "sweet":
+                    soup.seas = Seasoning.Sweet;
+                    break;
             }
 
-        static void ChangeChest()
-        {
-            Chest chestState = Chest.Open;
-            while (true) {
-            Console.WriteLine($"The chest is {chestState}.  What do you want to do?");
-            string currentRequest = Console.ReadLine();
-            if (currentRequest == null) {break;}
-            chestState = DetermineActionValidity(currentRequest, chestState);
-            }
+            DisplaySoup(soup);
+
         }
-}
+
+        static void DisplaySoup((SoupType soupt, MainIngredient mainingred, Seasoning season) soup)
+        {
+            Console.WriteLine($"{soup.season} {soup.mainingred} {soup.soupt}");
+        }
+    }
 }
---
+++ Program1.cs
@@ -0,0 +1,107 @@
+﻿// using System.Collections;
+// using System.Reflection.Metadata.Ecma335;
+// using System.Security.Cryptography;
+// using System.Security.Cryptography.X509Certificates;
+// using System.Xml.Serialization;
+
+// namespace HelloWorld
+// {
+//     class Program1
+//     {
+//         enum Chest { Open, Closed, Locked };
+
+//         static void Main(string[] args)
+//         {
+//             // Console.BackgroundColor = ConsoleColor.DarkCyan;
+            
+//             // Console.ForegroundColor = ConsoleColor.Black;
+//             // Console.Title = "Beautiousness"; 
+//             // Console.Beep();
+//             // Console.WriteLine("\"");
+//             // string name;
+//             // Console.WriteLine("What is your name?");
+//             // name = Console.ReadLine();
+//             // Console.WriteLine("Hi " + name);
+
+//             // Console.WriteLine("Target Row?");
+//             // string row = Console.ReadLine();
+//             // int numrow = Convert.ToInt32(row);
+//             // Console.WriteLine("Target Column?");
+//             // string col = Console.ReadLine();
+//             // int numcol = Convert.ToInt32(col);
+//             // Console.WriteLine($"({numrow}, {numcol - 1}) \n ({numrow - 1}, {numcol}) \n ({numrow}, {numcol + 1}) \n ({numrow + 1}, {numcol})");
+
+//             // Console.WriteLine("Enter a number");
+//             // string stringNum = Console.ReadLine();
+//             // int chosenNum = Convert.ToInt32(stringNum);
+//             // float chosenNumModulo = chosenNum % 2;
+//             // if (chosenNumModulo > 0)
+//             // {
+//             //     Console.WriteLine("Odd");
+//             // }
+//             // else
+//             // {
+//             //     Console.WriteLine("Even");
+//             // }
+//             //     int result = AskForNumber("what is the airspeed velocity of an unladen swallow?");
+//             //     Console.WriteLine(result); 
+
+//             // }
+
+//             // static int AskForNumber(string text)
+//             // {
+//             //     Console.WriteLine(text);
+//             //     string strnum = Console.ReadLine();
+//             //     int swallowNum = Convert.ToInt32(strnum);
+//             //     return swallowNum;
+//             // }
+//             ChangeChest();
+//         }
+
+//         static Chest DetermineActionValidity(string currentRequest, Chest chestState)
+//             {
+                
+//                 switch (currentRequest)
+//                 {
+//                     case "lock":
+//                         if (chestState == Chest.Closed)
+//                         {
+//                             return Chest.Locked;
+//                         }
+//                         break;
+//                     case "close":
+//                         if (chestState == Chest.Open)
+//                         {
+//                             return Chest.Closed;
+//                         }
+//                         break;
+//                     case "unlock":
+//                         if (chestState == Chest.Locked)
+//                         {
+//                             return Chest.Closed;
+//                         }
+//                         break;
+//                     case "open":
+//                         if (chestState == Chest.Closed)
+//                         {
+//                             return Chest.Open;
+//                         }
+//                         break;
+//                     default:
+//                         return chestState;
+//                 };
+//                 return chestState;
+//             }
+
+//         static void ChangeChest()
+//         {
+//             Chest chestState = Chest.Open;
+//             while (true) {
+//             Console.WriteLine($"The chest is {chestState}.  What do you want to do?");
+//             string currentRequest = Console.ReadLine();
+//             if (currentRequest == null) {break;}
+//             chestState = DetermineActionValidity(currentRequest, chestState);
+//             }
+//         }
+// }
+// }
