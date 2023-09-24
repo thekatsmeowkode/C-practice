acb202125de730ede89f63f07cd2f8f3d45fde37
Kat Munson <v-katmunson@microsoft.com>
2023-09-24 12:24:48 -0500

    Create color class

--- Program.cs
+++ Program.cs
@@ -13,6 +13,12 @@ namespace HelloWorld
             Point point2 = new Point(-4, 0);
             point1.printCoord();
             point2.printCoord();
+
+            Color white = Color.white;
+            Color customColor = new Color(234, 121, 2);
+            white.printColor();
+            customColor.printColor();
+
         }
 
         class Point
@@ -34,6 +40,31 @@ namespace HelloWorld
             public void printCoord() {
                 Console.WriteLine($"({this._x}, {this._y})");
             }
+        }
+
+        class Color {
+            public int _red;
+            public int _green;
+            public int _blue;
+
+            public Color(int red, int green, int blue) {
+                _red = red > 255 || red < 0 ? throw new ArgumentException("Property cannot be less than 0 or greater than 255") : red;
+                _green = green > 255 || green < 0 ? throw new ArgumentException("Property cannot be less than 0 or greater than 255") : green;
+                _blue = blue > 255 || blue < 0 ? throw new ArgumentException("Property cannot be less than 0 or greater than 255") : blue;
+            }
+
+            public void printColor() {
+                Console.WriteLine($"({this._red}, {this._green}, {this._blue})");
+            }
+
+            public static Color white = new Color(255, 255, 255);
+            public static Color black = new Color(255, 255, 255);
+            public static Color red = new Color(255, 255, 255);
+            public static Color orange = new Color(255, 255, 255);
+            public static Color yellow = new Color(255, 255, 255);
+            public static Color green = new Color(255, 255, 255);
+            public static Color blue = new Color(255, 255, 255);
+            public static Color purple = new Color(255, 255, 255);
         }
     }
 }
